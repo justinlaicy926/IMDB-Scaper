e7b3476b5e47f7c2d0e40b718ba9260d331c3ab4
justinlaicy926 <justinlaicy926@gmail.com>
2022-04-21 22:08:56 -0700

    Added Files

---
+++ IMDB_scraper/spiders/imdb_spider.py
@@ -0,0 +1,46 @@
+# to run 
+# scrapy crawl imdb_spider -o movies.csv
+
+import scrapy
+
+class ImdbSpider(scrapy.Spider):
+    name = 'imdb_spider'
+    
+    start_urls = ['https://www.imdb.com/title/tt0306414/']
+
+    def parse(self, response):
+        """
+        Parse method, navigates to the Cast segment of the IMDB page and calls the subsequent function
+        """
+
+        #creates new url for the credit page 
+        cast_link = response.urljoin("/fullcredits/")
+
+        #navigates to said page and call the appropriate function 
+        yield scrapy.Request(cast_link, callback= self.parse_full_credits)
+
+
+    def parse_full_credits(self,response):
+        """
+        Starts at a Cast page of IMDB, crawl all actors, crew not included, then call the parse_actor_page function 
+        """
+
+        #a list of relative paths for each actor   
+        rel_paths = [a.attrib["href"] for a in response.css("td.primary_photo a")]
+
+        #craws each link
+        for path in rel_paths:
+            actor_link = response.urljoin(path)
+            yield scrapy.Request(actor_link, callback = self.parse_full_credits)
+    
+    def parse_actor_page(self, response):
+        #selects actor name
+        actor_name = response.css("span.itemprop::text")
+
+        #element.css("::attr(id)")
+
+        #selects the work from the actor page
+        movie_or_TV_name = response.css("div.filmo-row b a::text")
+
+        yield {"actor" : actor_name, "movie_or_TV_name" : movie_or_TV_name}
+        
