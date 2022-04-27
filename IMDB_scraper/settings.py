c5e192b2236310a84e8f3909031da17f35f5bfd4
justinlaicy926 <justinlaicy926@gmail.com>
2022-04-27 15:38:09 -0700

    Update settings.py

--- IMDB_scraper/settings.py
+++ IMDB_scraper/settings.py
@@ -6,7 +6,7 @@
 #     https://docs.scrapy.org/en/latest/topics/settings.html
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-CLOSESPIDER_PAGECOUNT = 20
+#CLOSESPIDER_PAGECOUNT = 20
 
 BOT_NAME = 'IMDB_scraper'
 
